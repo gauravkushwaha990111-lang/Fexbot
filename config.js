ea5252ccc891a019d93127670e5c5fd25772253d
Gaurav <gauravkushwaha990111@gmail.com>
2026-04-03 14:26:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN || 
-    MONGO_URI: process.env.MONGO_URI || 
-    MAIN_ADMIN_ID: parseInt(process.env.MAIN_ADMIN_ID) ||  // Your Telegram ID here
-    ADMIN_PASS: process.env.ADMIN_PASS || 
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    MONGO_URI: process.env.MONGO_URI,
+    MAIN_ADMIN_ID: parseInt(process.env.MAIN_ADMIN_ID),
+    ADMIN_PASS: process.env.ADMIN_PASS
 };
